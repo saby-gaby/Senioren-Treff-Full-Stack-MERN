168e5c339a76f67c66fd0f0f4f7155ad671f3689
Dennis <dennis.piecha25@gmail.com>
2022-11-03 12:36:40 +0100

    strange 2. button

--- Frontend/src/components/landing/Landing.jsx
+++ Frontend/src/components/landing/Landing.jsx
@@ -4,8 +4,9 @@ const Landing = () => {
   return (
     <div className="container-fluid d-flex justify-content-center">
       <div className="row">
-        <div className="col-md-12">
+        <div className="col-md-30">
           <button className="btn">Veranstaltungen in deiner Nähe</button>
+          <button className="btn">Veranstaltungen erstellen</button>
         </div>
       </div>
     </div>
