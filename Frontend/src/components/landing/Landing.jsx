78e6e0d59647f7a3d2269e492d74055d7681e8de
Dennis <dennis.piecha25@gmail.com>
2022-11-03 14:25:40 +0100

    doch kein bootstrap

--- Frontend/src/components/landing/Landing.jsx
+++ Frontend/src/components/landing/Landing.jsx
@@ -2,13 +2,9 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div className="container-fluid d-flex justify-content-center">
-      <div className="row">
-        <div className="col-md-30">
-          <button className="btn">Veranstaltungen in deiner Nähe</button>
-          <button className="btn">Veranstaltungen erstellen</button>
-        </div>
-      </div>
+    <div>
+      <button>Veranstaltungen in deiner Nähe</button>
+      <button>Veranstaltungen erstellen</button>
     </div>
   );
 };
--- Frontend/src/components/nav/NavBar.jsx
+++ Frontend/src/components/nav/NavBar.jsx
@@ -2,18 +2,12 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-5">
-          <div className="logo">
-            <a href="/">Renter Treff</a>
-          </div>
-        </div>
-        <div className="auth-btns col-md-7">
-          <button className="btn sign-up">Sign Up</button>
-          <button className="btn sign-in">Sign In</button>
-        </div>
+    <div>
+      <div className="logo">
+        <img src="logo.svg"></img>
       </div>
+      <button>Sign Up</button>
+      <button>Sign In</button>
     </div>
   );
 };
