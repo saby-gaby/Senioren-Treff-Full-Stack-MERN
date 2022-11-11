69cd24e1d9beffd251a5ff1c6acd609a0a77860f
Holger <holgeraliusklose@gmail.com>
2022-11-11 09:31:36 +0100

    set cookie is logged

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -8,14 +8,13 @@ export const createUser = async (req, res) => {
 
     const newUserData = await UserModel.create({
       userName: req.body.userName,
-      firstName:req.body.firstName,
-      lastName:req.body.lastName,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
       email: req.body.email,
       password: hashedSaltyPassword,
       location: req.body.location,
     });
-    
-    
+
     res.status(201).send(newUserData);
   } catch (error) {
     res.status(401).send(error.message);
@@ -25,46 +24,58 @@ export const createUser = async (req, res) => {
 export const userLogin = async (req, res) => {
   const loginUser = await UserModel.findOne({ userName: req.body.userName });
   if (!loginUser) {
-    return res.send({ error: "user/password combination not found" })
+    return res.send({ error: "user/password combination not found" });
   }
-  const isRightPassword = await bcrypt.compare(req.body.password, loginUser.password)
+  const isRightPassword = await bcrypt.compare(
+    req.body.password,
+    loginUser.password
+  );
 
   if (!isRightPassword) {
     return res.send({ error: "user/password combination not found" });
-  };
+  }
 
-  const expiresInSec = 1 * 60 * 60 * 24; // 1 h * 24 => 24h 
+  const expiresInSec = 1 * 60 * 60 * 24; // 1 h * 24 => 24h
 
-  const token = jwt.sign({ userName: loginUser.userName, userId: loginUser._id }, process.env.TOKEN_SECRET, { expiresIn: expiresInSec });
+  const token = jwt.sign(
+    { userName: loginUser.userName, userId: loginUser._id },
+    process.env.TOKEN_SECRET,
+    { expiresIn: expiresInSec }
+  );
 
-  res.cookie("jwt", token, { httpOnly: false, maxAge: expiresInSec * 1000 });
+  res.cookie("jwt", token, { httpOnly: true, maxAge: expiresInSec * 1000 });
 
-  return res.send({ msg: "successfully logged in", userName: loginUser.userName, userId: loginUser._id});
+  const expireDate = new Date().getTime() + expiresInSec;
+
+  res.cookie("isLogged", expireDate, {
+    httpOnly: false,
+    maxAge: expiresInSec * 1000,
+  });
+
+  return res.send({
+    msg: "successfully logged in",
+    userName: loginUser.userName,
+    userId: loginUser._id,
+  });
 };
 
 export const userLogout = (req, res) => {
   res.clearCookie("jwt");
   res.clearCookie("isLogged");
   return res.status(200).json({ msg: "successfully logged out" });
-}; 
-
-
+};
 
 export const getUserByID = async (req, res) => {
   try {
-    const getUser = await UserModel
-      .findById(req.params.id)
+    const getUser = await UserModel.findById(req.params.id)
       .populate("myEvents")
       .populate("watchedEvents");
-    
-    res.status(302).send(getUser);
 
+    res.status(302).send(getUser);
   } catch (error) {
     res.status(404).send(error.message);
   }
 };
-
-
 
 export const deleteUserByID = async (req, res) => {
   try {
