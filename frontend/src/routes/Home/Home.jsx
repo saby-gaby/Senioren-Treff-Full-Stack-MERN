39dd3328697124757067dab9f4f6245cf4584d26
Dennis <dennis.piecha25@gmail.com>
2022-11-15 10:56:11 +0100

    gogo

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -1,23 +1,31 @@
 import React, { useContext, useState, useRef, useEffect } from "react";
 import axiosConfig from "../../util/axiosConfig";
 import { SectionsContext } from "../../context/sectionsContext";
+import { Navigate, NavLink } from "react-router-dom";
+
 export default function Home() {
   const { foundEvents, setFoundEvents } = useContext(SectionsContext);
   const formElement = useRef(null);
   const locationElement = useRef(null);
+  const [found, setFound] = useState(false);
   const submitHandler = async (e) => {
     e.preventDefault();
     const location = locationElement.current.value;
     try {
       const axiosResp = await axiosConfig.get(`/search/${location}`);
-      setFoundEvents(axiosResp.data);
+      if (axiosResp.data[0]) {
+        setFound(true);
+        setFoundEvents(axiosResp.data);
+      }
     } catch (error) {}
   };
   useEffect(() => {
     console.log(foundEvents);
-  }, [foundEvents]);
+  }, [found]);
+
   return (
     <div className="Home">
+      {found && <Navigate to="/events" replace={true} />}
       <div>
         <h1>Veranstaltungen in deiner Nähe</h1>
         <p>Wo bist du gerade?</p>
@@ -28,7 +36,7 @@ export default function Home() {
       </div>
       <div>
         <h2>Veranstaltung erstellen</h2>
-        <button>Erstellen</button>
+        <NavLink to="/event-form">Erstellen</NavLink>
       </div>
     </div>
   );
