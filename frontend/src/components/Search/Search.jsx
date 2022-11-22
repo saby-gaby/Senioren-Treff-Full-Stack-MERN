f81743e4485246594a35261f762e5a1b171ef484
Dennis <dennis.piecha25@gmail.com>
2022-11-22 15:39:15 +0100

    final buttttons

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -1,13 +1,13 @@
 import React, { useContext, useRef, useState } from "react";
 import axiosConfig from "../../util/axiosConfig";
 import { SectionsContext } from "../../context/sectionsContext";
-import './Search.css'
+import "./Search.css";
 
 export default function Search() {
   const { navigate, isAuth } = useContext(SectionsContext);
   const formElement = useRef(null);
   const locationElement = useRef(null);
-  const defSearch = localStorage.getItem("defSearch")
+  const defSearch = localStorage.getItem("defSearch");
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -17,29 +17,28 @@ export default function Search() {
 
     if (!location && !isAuth) {
       alert("bitte gib eine Stadt ein");
-    }else if(!location && isAuth){
+    } else if (!location && isAuth) {
       try {
         const axiosResp = await axiosConfig.get(`/search/${defSearch}`);
-        if (axiosResp.data[0]) { 
+        if (axiosResp.data[0]) {
           navigateToEvents(defSearch);
         } else {
           alert(`keine Veranstaltungen in ${defSearch} gefunden`);
         }
-      } catch (error) { 
-        alert(error)
+      } catch (error) {
+        alert(error);
       }
     } else {
       try {
         const axiosResp = await axiosConfig.get(`/search/${location}`);
-        if (axiosResp.data[0]) { 
+        if (axiosResp.data[0]) {
           navigateToEvents(location);
         } else {
           alert(`keine Veranstaltungen in ${location} gefunden`);
         }
-      } catch (error) { 
-        alert(error)
+      } catch (error) {
+        alert(error);
       }
-
     }
   };
 
@@ -53,8 +52,12 @@ export default function Search() {
         <h1>Veranstaltungen in deiner Nähe</h1>
         <p>Wo bist du gerade?</p>
         <form ref={formElement} method="" onSubmit={submitHandler}>
-          <input ref={locationElement} type="text" placeholder={isAuth ? defSearch : "Dein Ort"} />
-          <input className="button" type="submit" value="Los geht's!" />
+          <input
+            ref={locationElement}
+            type="text"
+            placeholder={isAuth ? defSearch : "Dein Ort"}
+          />
+          <input className="button-green" type="submit" value="Los geht's!" />
         </form>
       </div>
     </div>
