8aa6bce8d78f80e76ab27959c74e15db272871cd
Dennis <dennis.piecha25@gmail.com>
2022-11-28 15:31:10 +0100

    Final Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,48 +9,31 @@ export default function Footer() {
   };
 
   return (
-    <div>
+    <div className="Copyright">
       <button onClick={handleClick}>Copyright &copy; by us</button>
       {isShown && (
-        <div>
+        <>
           <h2>Our Team</h2>
-        </div>
-      )}
-      {isShown && (
-        <div>
-          <div>
-            <p>
-              Sabina
-              <button>
-                <img src="oma.svg" alt="" />
-              </button>
-            </p>
+
+          <div className="Footer">
+            <a href="https://github.com/saby-gaby" target="_blank">
+              <p>Sabina</p>
+              <img src="Sabina.jpeg" alt="" />
+            </a>
+            <a href="https://github.com/Holledrums" target="_blank">
+              <p>Holger</p>
+              <img src="Holger.jpeg" alt="" />
+            </a>
+            <a href="https://github.com/Tom-Necke" target="_blank">
+              <p>Tom</p>
+              <img src="Tom.jpeg" alt="" />
+            </a>
+            <a href="https://github.com/DennisPiecha" target="_blank">
+              <p>Dennis</p>
+              <img src="Dennis.jpeg" alt="" />
+            </a>
           </div>
-          <div>
-            <p>
-              Tom
-              <button>
-                <img src="oma.svg" alt="" />
-              </button>
-            </p>
-          </div>
-          <div>
-            <p>
-              Holger
-              <button>
-                <img src="oma.svg" alt="" />
-              </button>
-            </p>
-          </div>
-          <div>
-            <p>
-              Dennis
-              <button>
-                <img src="oma.svg" alt="" />
-              </button>
-            </p>
-          </div>
-        </div>
+        </>
       )}
     </div>
   );
