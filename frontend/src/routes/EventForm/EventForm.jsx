dc65c7b35c32ac8ded5d7e203d417b86b1386dc0
saby-gaby <sgudzenova@gmail.com>
2022-12-13 11:03:58 +0100

    editEvent css

--- frontend/src/routes/EventForm/EventForm.jsx
+++ frontend/src/routes/EventForm/EventForm.jsx
@@ -207,7 +207,7 @@ export default function EventForm() {
                     handleChange(e);
                     setSpiele(!spiele);
                   }}
-                />{" "}
+                />
                 Spiele
               </label>
             </div>
